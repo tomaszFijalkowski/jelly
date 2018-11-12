39da9bed78dea6c821da0c63d9bb0a074bb07ced
Tomasz Fijalkowski <tomelecf@gmail.com>
2018-11-12 18:32:22 +0100

    Empty project

---
+++ TamagotchiProject/Assets/Scripts/Tamagotchi.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tamagotchi : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		Debug.Log("hello");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
