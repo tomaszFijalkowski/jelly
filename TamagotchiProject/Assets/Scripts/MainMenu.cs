fe74a407329ed5c6e89539927eab19db9a5c6a1d
Tomasz Fijalkowski <tomelecf@gmail.com>
2018-11-20 00:56:51 +0100

    Added main menu screen

---
+++ TamagotchiProject/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject transitionPanel;
+
+    public void Play()
+    {
+        transitionPanel.GetComponent<Animator>().Play("SceneEnd");
+        StartCoroutine(SwitchScene());
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    private IEnumerator SwitchScene()
+    {
+        yield return new WaitForSeconds(0.25f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ TamagotchiProject/Assets/Scripts/MainMenuButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuButton : MonoBehaviour {
+	
+	public void MenuButtonEnter()
+	{
+		GetComponent<Animation>().Play("MenuButtonEnter");
+	}
+	
+	public void MenuButtonExit()
+	{
+		GetComponent<Animation>().Play("MenuButtonExit");
+	}
+}
